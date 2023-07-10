d997daefd631c42ad7961d789d5d33a73a581fe1
JuanDuran0307 <juanalexanderquinchoa@gmail.com>
2023-07-10 09:22:06 -0500

    first commit

---
+++ Backend-Node-Mongo/index.js
@@ -0,0 +1,16 @@
+import express  from "express";
+import dotenv from "dotenv";
+import conectarDB from "./config/config.js";
+
+const app = express();
+
+dotenv.config();
+
+const port  = process.env.PORT;
+
+
+conectarDB();
+
+app.listen(port, ()=>{
+    console.log(`server was runnning on ${port}`);
+})
